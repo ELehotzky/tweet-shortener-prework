9677d714a2ff8aa76e605e6902ed00d3788fba0c
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-14 21:03:49 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -35,11 +35,9 @@ def bulk_tweet_shortener(tweets)
   }
 end
 
-def selective_tweet_shortener
+def selective_tweet_shortener(tweet)
   if tweet.length > 140
     word_substituter(tweet)
-  else
-    tweet
   end
 end
 
